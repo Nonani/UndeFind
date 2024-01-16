cccd936c73da1bf627a398a133a7f24d49137613
wschoi0329 <usunga2@gmail.com>
2024-01-16 16:53:55 +0900

    feat : phone_book_screen 완료

--- lib/presentation/screens/phone_book_screen.dart
+++ lib/presentation/screens/phone_book_screen.dart
@@ -1,11 +1,154 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
-class PhoneBookScreen extends StatelessWidget {
-  const PhoneBookScreen({super.key});
+class PhoneBookScreen extends StatefulWidget {
+  const PhoneBookScreen({Key? key}) : super(key: key);
 
+  @override
+  _PhoneBookScreenState createState() => _PhoneBookScreenState();
+}
+
+class _PhoneBookScreenState extends State<PhoneBookScreen> {
+  List<Contact> contacts = [];
+
+  TextEditingController searchController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    contacts = buildContacts();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final sizeX = MediaQuery.of(context).size.width;
+    final sizeY = MediaQuery.of(context).size.height;
+
+    return Container(
+      width: sizeX,
+      height: sizeY,
+      color: Colors.black,
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: 100,
+              color: Colors.black,
+              padding: EdgeInsets.only(top: 30, left: 5),
+              child: Row(
+                children: [
+                  Align(
+                    alignment: Alignment.centerLeft, // 왼쪽 정렬을 위해 Alignment.centerLeft 사용
+                    child: Image.asset('assets/images/logo.png', color: Colors.white),
+                  ),
+                ],
+              )
+            ),
+            Container(
+              padding: EdgeInsets.only(top: 30),
+              child: TextField(
+                controller: searchController,
+                onChanged: (value) {
+                  filterContacts(value);
+                },
+                decoration: InputDecoration(
+                  hintText: '검색어를 입력해주세요.',
+                  border: InputBorder.none,
+                  icon: Padding(
+                    padding: EdgeInsets.only(left: 13),
+                    child: Icon(Icons.search),
+                  ),
+                ),
+              ),
+              height: 100,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(30),
+                  topRight: Radius.circular(30),
+                ),
+              ),
+            ),
+            Divider(
+              height: 1,
+              color: Colors.grey,
+            ),
+            Container(
+              color: Colors.white,
+              width: sizeX,
+              height: sizeY,
+              child: ListView.separated(
+                itemCount: contacts.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return ListTile(
+                    title: Text(contacts[index].name),
+                    subtitle: Text(contacts[index].number),
+                    leading: CircleAvatar(
+                      child: Icon(contacts[index].icon),
+                    ),
+                    trailing: IconButton(
+                      icon: Icon(Icons.call),
+                      onPressed: () {
+                        launchUrl(Uri.parse('tel:' + contacts[index].number));
+                      },
+                    ),
+                  );
+                },
+                separatorBuilder: (BuildContext context, int index) {
+                  return Divider(
+                    height: 1,
+                    color: Colors.grey,
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
+
+  void launchUrl(Uri uri) async {
+    if (await canLaunch(uri.toString())) {
+      await launch(uri.toString());
+    } else {
+      throw 'Could not launch $uri';
+    }
+  }
+
+  List<Contact> buildContacts() {
+    List<Contact> contacts = [];
+    // 전화번호부
+
+    for (int i = 0; i< 20; i++) {
+      contacts.add(Contact(
+          '제주청 해양오염방제과', '064-801-2591', Icons.sentiment_very_satisfied));
+      contacts.add(Contact(
+          '제주시 해양오염방제과', '064-766-2591', Icons.sentiment_very_satisfied));
+      contacts.add(Contact(
+          '서귀포시 해양오염방제과', '064-793-2591', Icons.sentiment_very_satisfied));
+    }
+    return contacts;
+  }
+
+  void filterContacts(String query) {
+    setState(() {
+      if (query.isEmpty) {
+        contacts = buildContacts();
+      } else {
+        contacts = buildContacts()
+            .where((contact) =>
+        contact.name.toLowerCase().contains(query.toLowerCase()) ||
+            contact.number.contains(query))
+            .toList();
+      }
+    });
+  }
+}
+
+class Contact {
+  String name;
+  String number;
+  IconData icon;
+  Contact(this.name, this.number, this.icon);
 }
